d7315a501d5cfcabc2bdf06f8cbfd736c6f0187d
Viktor Sharov <Viktor.Sharov@harmonicinc.com>
2024-09-13 18:22:18 +0300

    Initial commit

--- kt_football_bot.py
+++ kt_football_bot.py
@@ -4,12 +4,27 @@ KT Football bot
 """
 
 import logging
+import os.path
+import sys
+
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
+
 def main() -> None:
-    app = ApplicationBuilder().token("YOUR TOKEN HERE").build()
+    tg_bot_token_file = "tg_bot_token"
+    if len(sys.argv) > 1:
+        tg_bot_token_file = sys.argv[1]
+    if not os.path.exists(tg_bot_token_file):
+        logging.error(
+            f"Unable to find {tg_bot_token_file} with Telegram bot token. Provide file name as first argument to script"
+        )
+        return
+    with open(tg_bot_token_file) as f:
+        token = f.read()
+    app = ApplicationBuilder().token(token).build()
     app.run_polling()
+
 
 if __name__ == "__main__":
     main()
