5ead8c3c8f87a95a92ff9dddb045012e45fe6c52
Viktor Sharov <Viktor.Sharov@harmonicinc.com>
2024-09-16 11:10:31 +0300

    Start using webhook

--- kt_football_bot.py
+++ kt_football_bot.py
@@ -3,21 +3,34 @@ KT Football bot
 ©Viktor Sharov, 2024
 """
 
+import asyncio
 import logging
 import os.path
 import signal
 import sys
 
 import json
+import time
+
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
+
+async def regular_task():
+    for i in range(0, 20):
+        print(f"Note: call from regular task {time.monotonic()}")
+        await asyncio.sleep(5.7)
+
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text(f'Hi! Please add this bot to group!')
+    await update.message.reply_text(f"Hi! Please add this bot to group!")
 
 
 async def test_echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text(f'Echo')
+    logging.info(repr(update.message.chat))
+    await context.bot.send_message(
+        update.message.chat_id, "Message <b>with</b> echo reply"
+    )
 
 
 def main() -> None:
@@ -37,6 +50,8 @@ def main() -> None:
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CommandHandler("echo", test_echo))
     logging.info("Run Telegram Bot webhook...")
+    event_loop = asyncio.get_event_loop()
+    event_loop
     app.run_webhook(
         listen=credentials["web_addr"] if "web_addr" in credentials else "0.0.0.0",
         port=credentials["web_port"] if "web_addr" in credentials else 80,
@@ -44,8 +59,8 @@ def main() -> None:
         cert=credentials["cert"],
         key=credentials["key"],
         webhook_url=credentials["web_hook_url"],
-        stop_signals = [signal.SIGTERM, signal.SIGINT],
-        secret_token = credentials["web_hook_token"],
+        stop_signals=[signal.SIGTERM, signal.SIGINT],
+        secret_token=credentials["web_hook_token"],
     )
 
 
