a7dc1dc066ea76932fbec9e45a0741f427b3f09c
Viktor Sharov <Viktor.Sharov@harmonicinc.com>
2024-09-16 10:47:51 +0300

    Start using webhook

--- kt_football_bot.py
+++ kt_football_bot.py
@@ -5,25 +5,48 @@ KT Football bot
 
 import logging
 import os.path
+import signal
 import sys
 
+from pandas.io import json
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f'Hi! Please add this bot to group!')
+
+
+async def test_echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f'Echo')
+
 
 def main() -> None:
-    tg_bot_token_file = "tg_bot_token"
+    credentials_file = "credentials.json"
     if len(sys.argv) > 1:
-        tg_bot_token_file = sys.argv[1]
-    if not os.path.exists(tg_bot_token_file):
+        credentials_file = sys.argv[1]
+    if not os.path.exists(credentials_file):
         logging.error(
-            f"Unable to find {tg_bot_token_file} with Telegram bot token. Provide file name as first argument to script"
+            f"Unable to find {credentials_file} with Telegram bot token. Provide file name as first argument to script"
         )
         return
-    with open(tg_bot_token_file) as f:
-        token = f.read()
+
+    with open(credentials_file) as f:
+        credentials = json.loads(f.read())
+    token = credentials["tg_bot_token"]
     app = ApplicationBuilder().token(token).build()
-    app.run_polling()
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(CommandHandler("echo", test_echo))
+    logging.info("Run Telegram Bot webhook...")
+    app.run_webhook(
+        listen=credentials["web_addr"] if "web_addr" in credentials else "0.0.0.0",
+        port=credentials["web_port"] if "web_addr" in credentials else 80,
+        # url_path=credentials["web_path"],
+        cert=credentials["cert"],
+        key=credentials["key"],
+        webhook_url=credentials["web_hook_url"],
+        stop_signals = [signal.SIGTERM, signal.SIGINT],
+        secret_token = credentials["web_hook_token"],
+    )
 
 
 if __name__ == "__main__":
