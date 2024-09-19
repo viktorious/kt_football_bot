10c1a1b51e31425bd0493a54a24579187ecd5e49
Viktor Sharov <Viktor.Sharov@harmonicinc.com>
2024-09-19 14:32:01 +0300

    Adding database

---
+++ database.py
@@ -0,0 +1,40 @@
+"""
+SQLite database support functions for kt_football_bot bot
+"""
+import aiosqlite
+
+
+class FootballBotDatabase:
+
+    def __init__(self, file_name="kt_football.db"):
+        self.__db_file_name = file_name
+        self.__db = None
+    async def _get_db(self) -> aiosqlite.Connection:
+        if self.__db is None:
+            self.__db = await aiosqlite.connect(self.__db_file_name)
+            await self.__db.execute("create table if not exists event(id INTEGER PRIMARY KEY ASC AUTOINCREMENT, "
+                                    "event_title TEXT, "
+                                    "event_time REAL, "
+                                    "message_time REAL, "
+                                    "message_id INTEGER(8) NOT NULL"
+                                    "chat_id INTEGER(8) NOT NULL)")
+            await self.__db.execute("create table if not exists event_member(id PRIMARY KEY ASC AUTOINCREMENT, "
+                                    "event_id INTEGER NOT NULL, "
+                                    "user_id INTEGER(8) NOT NULL, "
+                                    "name text"
+                                    "username text"
+                                    "join_time REAL)")
+        return self.__db
+
+    async def create_event(self, event_title, event_time, message_time, message_id, chat_id):
+        db: aiosqlite.Connection = await self._get_db()
+        event_title = str(event_title).replace("'", "")
+        event_title = str(event_title).replace('"', "")
+        event_time = float(event_time)
+        message_time = float(message_time)
+        message_id = int(message_id)
+        chat_id = int(chat_id)
+        await db.execute(f"inset into event(event_title, event_time, message_time, message_id, chat_id) "
+                         f"values('{event_title}', '{event_time}', '{message_time}', '{message_id}', '{chat_id}')")
+        await db.commit()
+
--- kt_football_bot.py
+++ kt_football_bot.py
@@ -12,29 +12,51 @@ import sys
 import json
 import time
 
+from typing import Optional
+
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
+from database import FootballBotDatabase
+
 logger = logging.getLogger(__name__)
 
-current_periodic_task = None
+database: Optional[FootballBotDatabase] = None
 
-async def regular_task():
-    for i in range(0, 20):
-        logger.info(f"Note: call from regular task {time.monotonic()}")
-        await asyncio.sleep(5.7)
+# current_periodic_task = None
+
+# async def regular_task():
+#     for i in range(0, 20):
+#         logger.info(f"Note: call from regular task {time.monotonic()}")
+#         await asyncio.sleep(5.7)
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text(f"Hi! Please add this bot to group!")
+    await update.message.reply_text(f"Hello from KT Football Bot")
+
+
+async def kt_create_event(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f"...creating event...")
+    await database.create_event("NEW_EVENT_HERE", time.monotonic(), time.monotonic(), -1, update.message.chat_id)
+    msg = await context.bot.send_message(
+        update.message.chat_id, "<b>⚽️NEW_EVENT_HERE⚽️</b>\n"
+                                "\n"
+                                "1. Viktor Sharov (the_viktorious)\n"
+                                f"\t\t⏱0.12 seconds\n\n"
+                                "2. Viktor Sharov (the_viktorious)\n"
+                                f"\t\t⏱0.21 seconds\n\n",
+        parse_mode="HTML"
+    )
+    logger.info(f"Created message id is {msg.id}")
 
 
 async def test_echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    global current_periodic_task
-    if current_periodic_task is None:
-        event_loop = asyncio.get_event_loop()
-        current_periodic_task = event_loop.create_task(regular_task())
+    # global current_periodic_task
+    # if current_periodic_task is None:
+    #     event_loop = asyncio.get_event_loop()
+    #     current_periodic_task = event_loop.create_task(regular_task())
     logging.info(repr(update.message.chat))
+
     await context.bot.send_message(
         update.message.chat_id, "<b>👟NEW MESSAGE👟</b>\n"
                                 "\n"
@@ -50,6 +72,7 @@ async def test_echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
 def main() -> None:
 
+    global database
     logging.basicConfig(filename="kt_football_bot.log", level=logging.INFO)
 
     credentials_file = "credentials.json"
@@ -66,8 +89,9 @@ def main() -> None:
     token = credentials["tg_bot_token"]
     app = ApplicationBuilder().token(token).build()
     app.add_handler(CommandHandler("start", start))
-    app.add_handler(CommandHandler("echo", test_echo))
+    app.add_handler(CommandHandler("kt_add_event", kt_create_event))
     logging.info("Run Telegram Bot webhook...")
+    database = FootballBotDatabase(credentials["db_path"] if "db_path" in credentials else "kt_football.db")
     app.run_webhook(
         listen=credentials["web_addr"] if "web_addr" in credentials else "0.0.0.0",
         port=credentials["web_port"] if "web_addr" in credentials else 80,
@@ -78,9 +102,9 @@ def main() -> None:
         stop_signals=[signal.SIGTERM, signal.SIGINT],
         secret_token=credentials["web_hook_token"],
     )
-    global current_periodic_task
-    if current_periodic_task is None:
-        current_periodic_task.cancel()
+    # global current_periodic_task
+    # if current_periodic_task is None:
+    #     current_periodic_task.cancel()
 
 
 if __name__ == "__main__":
