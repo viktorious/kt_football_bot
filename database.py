511a9b363bbb2de7ad180ca52c6574a65aef686f
Viktor Sharov <Viktor.Sharov@harmonicinc.com>
2024-11-04 16:48:19 +0200

    Adding database

--- database.py
+++ database.py
@@ -1,5 +1,29 @@
 """
 SQLite database support functions for kt_football_bot bot
+Creates and supports tables in database:
+    - event
+      Event description:
+      - Title of event
+      - Time of event
+      - Address of event
+      - Timestamp of message - timestamp when message was created in chat;
+                               could be in future - in case message should be scheduled
+      - Chan id - in which chat event was created
+      - Limit of players - how many players takes participate in action; players beyond limit are added to queue
+    - event_member
+    Event participants:
+      - Event id
+      - user id (Telegram unique user id)
+      - name (user visible name taken from Telegram)
+      - username (user Telegram nick, if exists)
+      - timestamps of joining to create proper sequence of joined players
+      - count - how many times player clicked "join" in event message
+    Note: there is no "user" table - user information is taken from Telegram
+    - ban
+    Banned user list
+      - Chat id
+      - User id
+      -
 """
 import aiosqlite
 import logging
@@ -28,10 +52,21 @@ class FootballBotDatabase:
              "user_id INTEGER(8) NOT NULL, "
              "name text, "
              "username text, "
-             "join_timestamp REAL)"
+             "join_timestamp REAL,"
+             "count INTEGER default 1)"
+        ),
+        (
+            "create table if not exists "
+            "ban(id INTEGER PRIMARY KEY AUTOINCREMENT, "
+            "chat_id INTEGER NOT NULL, "
+            "user_id INTEGER(8) NOT NULL, "
+            "end_timestamp REAL)"
         ),
         "create index if not exists event_event_time_idx on event(event_time)",
+        "create index if not exists event_event_time_chat on event(chat_id)",
         "create unique index if not exists member_event_filter on event_member(event_id, user_id)",
+        "create index if not exists member_event_time_order on event_member(event_id, user_id, join_timestamp)",
+        "create index if not exists ban_user on ban(event_id, user_id, join_timestamp)",
     ]
 
     def __init__(self, file_name="kt_football.db"):
@@ -74,6 +109,27 @@ class FootballBotDatabase:
         primary_key = rows[0][0]
         await db.commit()
         return primary_key
+
+    async def update_event(self, event_id, event_descr):
+        """
+        Update event description in database
+        :param event_id: Event identifier in database
+        :param event_descr: Dictionary of items to update. Possible update:
+           'title', 'time', 'address', 'players_limit'
+        """
+        allowed_keys = {"title": "event_title",
+                        "time": "event_time",
+                        "address": "event_address",
+                        "players_limit": "players_limit"}
+        sql = []
+        for key, db_key in allowed_keys.items():
+            if key in event_descr:
+                s = str(event_descr[key]).replace("'", "").replace('"', "")
+                sql.append(f"{db_key}='{s}'")
+        if len(sql) > 0:
+            sql = f"update event set {','.join(sql)}"
+            db: aiosqlite.Connection = await self._get_db()
+            await db.execute(sql)
 
     async def get_all_events(self):
         db: aiosqlite.Connection = await self._get_db()
